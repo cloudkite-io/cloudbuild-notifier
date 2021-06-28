b6e2cc10a362bfbd09bb3eb13e7b1b9c9bc9ba07
Russell Nyorere <neorusse@gmail.com>
2021-06-28 21:07:31 +0100

    Refactor if statement

--- pkg/notifier/slack/slack.go
+++ pkg/notifier/slack/slack.go
@@ -32,32 +32,32 @@ func (n notifier) Send(cloudbuildResponse cloudbuildnotifier.CloudbuildResponse,
 
 	if cloudbuildResponse.Status == "QUEUED" || cloudbuildResponse.Status == "WORKING" {
 		return nil
-	} else {
-		attachment := slack.Attachment{
-			Title: fmt.Sprintf("Cloudbuild: %s", cloudbuildResponse.Status),
-			Color: color,
-			Text: fmt.Sprintf("Repo: %s\nBranch: %s\nCommit SHA: %s\nTrigger: %s\n",
-				buildParams.REPO_NAME, buildParams.BRANCH_NAME, buildParams.COMMIT_SHA, buildParams.TRIGGER_NAME),
-			Actions: []slack.AttachmentAction{
-				{
-					Text: "View Logs",
-					Type: "button",
-					URL:  cloudbuildResponse.LogURL,
-				},
-			},
-		}
-	
-		msg := slack.WebhookMessage{
-			Attachments: []slack.Attachment{attachment},
-		}
-	
-		err := slack.PostWebhook(n.webhookURL, &msg)
-		if err != nil {
-			return fmt.Errorf("failed posting to webhook %s: %s", n.webhookURL, err)
-		}
-	
-		log.Printf("Sent %s Slack message for build %s\n", n.webhookURL, buildParams.Id)
 	}
+
+	attachment := slack.Attachment{
+		Title: fmt.Sprintf("Cloudbuild: %s", cloudbuildResponse.Status),
+		Color: color,
+		Text: fmt.Sprintf("Repo: %s\nBranch: %s\nCommit SHA: %s\nTrigger: %s\n",
+			buildParams.REPO_NAME, buildParams.BRANCH_NAME, buildParams.COMMIT_SHA, buildParams.TRIGGER_NAME),
+		Actions: []slack.AttachmentAction{
+			{
+				Text: "View Logs",
+				Type: "button",
+				URL:  cloudbuildResponse.LogURL,
+			},
+		},
+	}
+
+	msg := slack.WebhookMessage{
+		Attachments: []slack.Attachment{attachment},
+	}
+
+	err := slack.PostWebhook(n.webhookURL, &msg)
+	if err != nil {
+		return fmt.Errorf("failed posting to webhook %s: %s", n.webhookURL, err)
+	}
+
+	log.Printf("Sent %s Slack message for build %s\n", n.webhookURL, buildParams.Id)
 
 	return nil
 }
