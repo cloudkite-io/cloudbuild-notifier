00c34b5efa545fe7ede13c7f081f95812954f4c5
Russell Nyorere <neorusse@gmail.com>
2021-06-28 20:37:02 +0100

    Remove queued & working slack msg

--- pkg/notifier/slack/slack.go
+++ pkg/notifier/slack/slack.go
@@ -19,6 +19,7 @@ func New(webhookURL string) cloudbuildnotifier.Notifier {
 }
 
 func (n notifier) Send(cloudbuildResponse cloudbuildnotifier.CloudbuildResponse, buildParams cloudbuild.BuildParameters) error {
+
 	var color string
 	switch {
 	case stringInSlice(cloudbuildResponse.Status, []string{"FAILURE", "INTERNAL_ERROR", "TIMEOUT"}):
@@ -29,30 +30,35 @@ func (n notifier) Send(cloudbuildResponse cloudbuildnotifier.CloudbuildResponse,
 		color = "good"
 	}
 
-	attachment := slack.Attachment{
-		Title: fmt.Sprintf("Cloudbuild: %s", cloudbuildResponse.Status),
-		Color: color,
-		Text: fmt.Sprintf("Repo: %s\nBranch: %s\nCommit SHA: %s\nTrigger: %s\n",
-			buildParams.REPO_NAME, buildParams.BRANCH_NAME, buildParams.COMMIT_SHA, buildParams.TRIGGER_NAME),
-		Actions: []slack.AttachmentAction{
-			{
-				Text: "View Logs",
-				Type: "button",
-				URL:  cloudbuildResponse.LogURL,
+	if cloudbuildResponse.Status == "QUEUED" || cloudbuildResponse.Status == "WORKING" {
+		return nil
+	} else {
+		attachment := slack.Attachment{
+			Title: fmt.Sprintf("Cloudbuild: %s", cloudbuildResponse.Status),
+			Color: color,
+			Text: fmt.Sprintf("Repo: %s\nBranch: %s\nCommit SHA: %s\nTrigger: %s\n",
+				buildParams.REPO_NAME, buildParams.BRANCH_NAME, buildParams.COMMIT_SHA, buildParams.TRIGGER_NAME),
+			Actions: []slack.AttachmentAction{
+				{
+					Text: "View Logs",
+					Type: "button",
+					URL:  cloudbuildResponse.LogURL,
+				},
 			},
-		},
+		}
+	
+		msg := slack.WebhookMessage{
+			Attachments: []slack.Attachment{attachment},
+		}
+	
+		err := slack.PostWebhook(n.webhookURL, &msg)
+		if err != nil {
+			return fmt.Errorf("failed posting to webhook %s: %s", n.webhookURL, err)
+		}
+	
+		log.Printf("Sent %s Slack message for build %s\n", n.webhookURL, buildParams.Id)
 	}
 
-	msg := slack.WebhookMessage{
-		Attachments: []slack.Attachment{attachment},
-	}
-
-	err := slack.PostWebhook(n.webhookURL, &msg)
-	if err != nil {
-		return fmt.Errorf("failed posting to webhook %s: %s", n.webhookURL, err)
-	}
-
-	log.Printf("Sent %s Slack message for build %s\n", n.webhookURL, buildParams.Id)
 	return nil
 }
 
